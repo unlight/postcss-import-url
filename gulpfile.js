b1ed8b26c5879fbe41b20e626e1a87250f684d21
roman.vasilev <root@localhost>
2019-01-03 12:37:15 +0400

    chore: Updated dependencies
    BREAKING CHANGE: Node 6+ is required now

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,38 @@
-var gulp = require("gulp");
-var connect = require("gulp-connect");
-var mocha = require("gulp-mocha");
-var eslint = require("gulp-eslint");
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var mocha = require('gulp-mocha');
+var eslint = require('gulp-eslint');
 
-var files = ["index.js", "gulpfile.js"];
+var files = ['index.js', 'gulpfile.js'];
 
-gulp.task("lint", function() {
+gulp.task('lint', function() {
 	return gulp.src(files)
 		.pipe(eslint())
 		.pipe(eslint.format());
 	// .pipe(eslint.failAfterError());
 });
 
-gulp.task("test", function() {
-	gulp.start("start-server");
-	return gulp.src("test/*.js", {read: false})
+gulp.task('test', function() {
+	var startServer = gulp.task('start-server');
+	startServer();
+	return gulp.src('test/*.js', {read: false})
 		.pipe(mocha({timeout: 5000}))
-		.on("end", connect.serverClose);
+		.on('end', connect.serverClose);
 });
 
-gulp.task("default", ["lint", "test"]);
+gulp.task('default', gulp.series(['lint', 'test']));
 
-gulp.task("watch", function() {
-	gulp.watch(files, ["lint", "test"]);
+gulp.task('watch', function() {
+	gulp.watch(files, ['lint', 'test']);
 });
 
-gulp.task("start-server", function() {
+gulp.task('start-server', function() {
 	connect.server({
-		root: "./test",
+		root: './test',
 		port: 1234
 	});
 });
 
-gulp.task("close-server", function() {
+gulp.task('close-server', function() {
 	connect.serverClose();
 });
