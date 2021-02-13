8405f32b9aa09bef534e67717bf7de3d52477547
Roman_Vasilev <s91630277@gmail.com>
2021-02-13 15:08:14 +0400

    fix: Set element source property
    fix #27

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,10 @@
 module.exports = {
-    printWidth: 100,
+    printWidth: 88,
     trailingComma: 'all',
     tabWidth: 4,
     semi: true,
     singleQuote: true,
+    arrowParens: 'avoid',
     overrides: [
         {
             files: '*.{json,yml}',
